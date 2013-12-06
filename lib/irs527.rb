ad39a3aa4a8e3f7b18454f035c235812ba734cf2
Derek Willis <dwillis@gmail.com>
2013-12-06 10:47:59 -0500

    added parser code

--- lib/irs527.rb
+++ lib/irs527.rb
@@ -1,5 +1,31 @@
 require "irs527/version"
 
 module Irs527
-  # Your code goes here...
+
+  class TextParser
+
+    def self.parse(path)
+      file_8871 = File.open('8871.txt', 'w')
+      file_8872 = File.open('8872.txt', 'w')
+      file_skeda = File.open('skeda.txt', 'w')
+      file_skedb = File.open('skedb.txt', 'w')
+      f = File.open(path).readlines
+      f.each do |line|
+        if line[0..1] == "1|"
+          file_8871.write(line)
+        elsif line[0..1] == "2|"
+          file_8872.write(line)
+        elsif line[0..1] == "A|"
+          file_skeda.write(line)
+        elsif line[0..1] == "B|"
+          file_skedb.write(line)
+        end
+      end
+      file_8871.close
+      file_8872.close
+      file_skeda.close
+      file_skedb.close
+    end
+  end
+
 end
