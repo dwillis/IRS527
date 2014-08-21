f36002baa4de0ace2ba91317188207ec840a8a8a
Matt Long <matthewhl@gmail.com>
2014-08-21 14:16:10 -0400

    add logic for invalid/truncated lines

--- lib/irs527/form_8872.rb
+++ lib/irs527/form_8872.rb
@@ -1,5 +1,8 @@
 module Irs527
   class Form8872 < Form
+    attr_accessor :line
+    attr_reader :sched_b_forms, :sched_a_forms
+
     HEADERS = [:record_type, :form_type, :form_id, :period_beg_date, :period_end_date,
       :init_rpt, :amend_rpt, :final_rpt, :change_of_addr, :name, :ein]
 
@@ -10,6 +13,8 @@ module Irs527
     def initialize(line, properties)
       @line       = line
       @properties = properties
+      @sched_a_forms = []
+      @sched_b_forms = []
     end
 
     def parse_properties
@@ -22,6 +27,14 @@ module Irs527
           footer(@line[value])
         end
       end
+    end
+
+    def truncated?
+      @line.length < 49
+    end
+
+    def truncated=(missing_fields)
+      @line.concat(missing_fields)
     end
 
     def parse_address(category, sub_hash)
@@ -88,6 +101,44 @@ module Irs527
       HEADERS.each_with_index do |h,i|
         format(h, head[i]) { |field| instance_variable_set("@#{h}", field) }
       end
+    end
+
+    def a_record=(supp_line)
+      @sched_a_forms << {
+        record_type: supp_line[0],
+        form_id: supp_line[1],
+        sched_a_id: supp_line[2],
+        org_name: supp_line[3],
+        ein: supp_line[4],
+        contrib_name: supp_line[5],
+        addr: addr("A", supp_line[6..11]),
+        employer: supp_line[12],
+        contrib_amt: supp_line[13].to_f,
+        contrib_occupation: supp_line[14],
+        agg_contrib_ytd: supp_line[15],
+        date: format(:date, supp_line[16])
+      }
+    end
+
+    def b_record=(supp_line)
+      @sched_b_forms << {
+        record_type: supp_line[0],
+        form_id: supp_line[1],
+        sched_b_id: supp_line[2],
+        org_name: supp_line[3],
+        ein: supp_line[4],
+        recip_name: supp_line[5],
+        addr: addr("B", supp_line[6..11]),
+        employer: supp_line[12],
+        expenditure_amt: supp_line[13].to_f,
+        recip_occupation: supp_line[14],
+        date: format(:date, supp_line[15]),
+        expenditure_purpose: supp_line[16]
+      }
+    end
+
+    def incomplete?
+      !@line.nil?
     end
   end
 end
