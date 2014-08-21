d8f6a555cc4e44311ee12b86d40113762510d610
Matt Long <matthewhl@gmail.com>
2014-08-21 11:07:01 -0400

    form finalization for 8872; sched A/B to follow

--- lib/irs527/form_8872.rb
+++ lib/irs527/form_8872.rb
@@ -1,7 +1,7 @@
 module Irs527
-  class Form8872
+  class Form8872 < Form
     HEADERS = [:record_type, :form_type, :form_id, :period_beg_date, :period_end_date,
-      :amend_rpt, :final_rpt, :init_rpt, :change_of_addr, :name, :ein]
+      :init_rpt, :amend_rpt, :final_rpt, :change_of_addr, :name, :ein]
 
     FOOTERS = [:qtr_indicator, :monthly_rpt, :pre_elect, :elect_date, :elect_state, :sched_a,
       :sched_b, :total_sched_a, :total_sched_b, :date
@@ -10,6 +10,32 @@ module Irs527
     def initialize(line, properties)
       @line       = line
       @properties = properties
+    end
+
+    def parse_properties
+      @properties.each do |property,value|
+        if value.is_a?(Hash)
+          parse_address(property, value)
+        elsif property == :header
+          header(@line[value])
+        else
+          footer(@line[value])
+        end
+      end
+    end
+
+    def parse_address(category, sub_hash)
+      sub_hash.each do |k,v|
+        if k == :addr
+          address = addr(category, @line[v])
+          address.each do |sub_cat, val|
+            val = format(sub_cat, val)
+            instance_variable_set("@#{sub_cat}", val)
+          end
+        else
+          instance_variable_set("@#{k}", @line[v])
+        end
+      end
     end
 
     def quarter_indicator(value)
@@ -35,7 +61,7 @@ module Irs527
       end
     end
 
-    def month(val)
+    def monthly(val)
       Date::ABBR_MONTHNAMES[val.to_i]
     end
 
@@ -60,8 +86,7 @@ module Irs527
 
     def header(head)
       HEADERS.each_with_index do |h,i|
-        val = format(h, head[i])
-        define_var(h, val)
+        format(h, head[i]) { |field| instance_variable_set("@#{h}", field) }
       end
     end
   end
