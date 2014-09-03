377b5577ddd83291512b2f28d38f96da33cc7c10
Matt Long <matthewhl@gmail.com>
2014-09-02 20:52:43 -0400

    alter queries to respond to new format

--- lib/irs527/form_list.rb
+++ lib/irs527/form_list.rb
@@ -8,11 +8,15 @@ module Irs527
 
       CSV.foreach(csv_file) do |row|
         ein = row.shift
+        name = row.shift
 
-        form_paths[ein] = ->() {
-          row.each_slice(2).map do |form|
-            Utility.parse_form(IO.read(file, form[0].to_i, form[1].to_i))
-          end
+        form_paths[ein] = {
+          name: name,
+          forms: ->() {
+            row.each_slice(2).map do |form|
+              Utility.parse_form(IO.read(file, form[0].to_i, form[1].to_i))
+            end
+          }
         }
       end
 
@@ -24,42 +28,37 @@ module Irs527
       @file = file
     end
 
+    def search_by_name(name)
+      result = []
+      @forms.keys.each do |ein|
+        result << ein if @forms[ein][:name] =~ Regexp.new(name)
+      end
+
+      return result.map { |ein| @forms[ein][:forms].call }.flatten
+    end
+
+    def search_by_ein(ein)
+      @forms[ein][:forms].call if @forms[ein]
+    end
+
     def most_recent_non_amend(ein)
-      non_amended(ein).max { |form| form.date }
+      non_amended(ein).max { |form| form.date } if @forms[ein]
     end
 
     def non_amended(ein)
-      @forms[ein].select { |form| form.non_amend? }
-    end
-
-    def all_non_amended
-      # Expensive method. Do not use this.
-      non_amended = []
-      @forms.each do |ein,form_list|
-        @forms[ein] = form_list.call if form_list.is_a?(Proc)
-        non_amended << form_list.select { |form| form.non_amend? }
-      end
-
-      non_amended
+      @forms[ein][:forms].call.select { |form| form.non_amend? } if @forms[ein]
     end
 
     def eins
-      @forms.keys
+      @forms.keys.map { |ein| { name: @forms[ein][:name], ein: ein } }
     end
 
     def sum_contributions(ein)
-      non_amended(ein).inject { |x,y| x.contrib_total + y.contrib_total }
-    end
-
-    def [](ein)
       if @forms[ein]
-        if @forms[ein].is_a?(Proc)
-          @forms[ein] = @forms[ein].call
-        else
-          @forms[ein]
-        end
+        form_set = @forms[ein][:forms].call
+        form_set.select { |form| form.non_amend? }.inject { |x,y| x.total_sched_a + y.total_sched_b }
       else
-        puts "#{ein} not found."
+        0.0
       end
     end
   end
