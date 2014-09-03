472cb152eeea7433bf54d56a652ca8f457042f66
Matt Long <matthewhl@gmail.com>
2014-09-02 20:50:52 -0400

    add name to searchable field

--- lib/irs527.rb
+++ lib/irs527.rb
@@ -10,16 +10,18 @@ require "zip"
 module Irs527
   class Utility
     def self.retrieve_data(path)
-      Net::HTTP.start("forms.irs.gov") do |http|
-        req = Net::HTTP::Get.new("/app/pod/dataDownload/fullData")
-        size = 0
-        http.request(req) do |res|
-          open("#{path}/data.zip", "w") do |io|
-            res.read_body do |chunk|
-              size += chunk.bytesize
-              io.write(chunk)
-              if Time.now.sec % 30 == 0
-                puts "#{size.fdiv(1024**2)} MB d/l'ed"
+      if !File.file?("#{path}/data.zip")
+        Net::HTTP.start("forms.irs.gov") do |http|
+          req = Net::HTTP::Get.new("/app/pod/dataDownload/fullData")
+          size = 0
+          http.request(req) do |res|
+            open("#{path}/data.zip", "w") do |io|
+              res.read_body do |chunk|
+                size += chunk.bytesize
+                io.write(chunk)
+                if Time.now.sec % 30 == 0
+                  puts "#{size.fdiv(1024**2)} MB d/l'ed"
+                end
               end
             end
           end
@@ -28,12 +30,13 @@ module Irs527
 
       Zip::File.open("#{path}/data.zip") do |zip|
         zip.each do |entry|
-          @file = entry.name
-          entry.extract("./#{entry.name}")
-          puts "File extracted to #{path} as #{entry.name}"
+          @file = File.basename(entry.name)
+          entry.extract("#{path}/#{File.basename(entry.name)}")
+          puts "File extracted to #{path} as #{@file}"
         end
       end
 
+      File.delete("#{path}/data.zip")
       return @file
     end
 
@@ -48,30 +51,30 @@ module Irs527
         offset = file.pos
         line = file.readline.encode('UTF-8', invalid: :replace, replace: ' ')
 
-        # file_size.call(offset)
-
         if line[0..1] == "1|" || line[0..1] == "2|"
           form = Form.new(line.split("|"))
           @ein = form.type[:ein]
-
+          name = form.type[:name]
           if records[@ein]
-            records[@ein] << { offset: offset, length: line.length }
+            records[@ein][:forms] << { offset: offset, length: line.length }
           else
-            records[@ein] = [{ offset: offset, length: line.length }]
+            records[@ein] = {forms: [{ offset: offset, length: line.length }], name: name }
           end
         else
-          records[@ein][-1][:length] += line.length
+          records[@ein][:forms][-1][:length] += line.length
         end
       end
 
       CSV.open("#{output}/record_index.csv", "w") do |csv|
-        records.each do |ein,forms|
-          csv << [ein] + forms.map { |form| [ form[:length], form[:offset] ] }.flatten
+        records.each do |ein,entry|
+          name = entry[:name]
+          forms = entry[:forms]
+          csv << [ein, name] + forms.map { |form| [ form[:length], form[:offset] ] }.flatten
         end
       end
 
       file.close
-      FormList.load("#{output}/record_index.csv", path)
+      FormList.load("record_index.csv", path)
     end
 
     def self.parse_form(data_chunk)
@@ -91,6 +94,7 @@ module Irs527
         end
       end
 
+      primary_form.clear_line
       return primary_form
     end
   end
